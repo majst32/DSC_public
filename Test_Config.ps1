fc057c4e3c682de4a6c5f7f0b5d1b20fa052fbc5
Missy Januszko <majst32@noreply.github.com>
2016-04-13 14:06:07 -0400

    Added config data

--- Test_Config.ps1
+++ Test_Config.ps1
@@ -1,52 +1,43 @@
 ﻿Configuration SSLWMF5Config {
-    
-    param (
-        [parameter(Mandatory=$True)]
-        [string] $Guid,
-
-        [parameter(Mandatory=$True)]
-        [string[]] $SchannelSubkey
-    )
-
+   
     Import-DscResource -Name xHotfix -ModuleName xWindowsUpdate
 
-    Node $guid {
-    
-    foreach ($s in $SchannelSubkey) {
+        Node $AllNodes.NodeName {
+       
+            foreach ($S in $Node.SChannelSubkey) {
 
-        Registry $s {
-            Ensure = 'Present'
-            Key = "HKEY_Local_Machine\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\$s"
-            ValueName = "Enabled"
-            ValueType = 'Dword'
-            ValueData = "0"
-        }
-    }
-
+                Registry $S {
+                    Ensure = 'Present'
+                    Key = "HKEY_Local_Machine\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\$S)"
+                    ValueName = "Enabled"
+                    ValueType = 'Dword'
+                    ValueData = "0"
+                    }
+            }
         #Know this probably won't work if WMF5 isn't already on the target box
         #only using it for depends on example
-        File fWMFv5 {
-            Type = 'Directory'
-            Ensure = 'Present'
-            SourcePath ="\\fileshare\Files"
-            DestinationPath = "C:\_File_Area"
-            MatchSource = $True
-            Recurse = $True
-            #would like to include checksum
-        }
+                File fWMFv5 {
+                Type = 'Directory'
+                Ensure = 'Present'
+                SourcePath ="\\fileshare\Files"
+                DestinationPath = "C:\_File_Area"
+                MatchSource = $True
+                Recurse = $True
+                #would like to include checksum
+                }
 
         xHotfix hfWMF5 {
             Id = 'KB3134758'
             Path ="C:\_File_Area\Windows Management Framework 5.0 RTM\Win8.1AndW2K12R2-KB3134758-x64.msu"
             Ensure = 'Present'
             DependsOn = '[File]fWMFv5'
-        }
+            }
 
     }
 }
 
 
-$Guid=Get-DscLocalConfigurationManager -CimSession s1| Select-Object -ExpandProperty ConfigurationID
-SSLWMF5Config -guid  $Guid -SchannelSubkey 'SSL 2.0\Server','SSL 3.0\Client','SSL 3.0\Server' -OutputPath "C:\DSC\Config"
+
+SSLWMF5Config -ConfigurationData "C:\powershell\config_data_test.psd1" -OutputPath "C:\DSC\Config"
 
      
