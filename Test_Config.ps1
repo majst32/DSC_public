a0da33d885b50fb620b64f7e79c7210b503d0eb8
Missy Januszko <majst32@noreply.github.com>
2016-04-13 14:00:20 -0400

    Added DependsOn

--- Test_Config.ps1
+++ Test_Config.ps1
@@ -12,16 +12,36 @@
 
     Node $guid {
     
-        foreach ($s in $SchannelSubkey) {
-            
-            Registry $s {
-                Ensure = 'Present'
-                Key = "HKEY_Local_Machine\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\$s"
-                ValueName = "Enabled"
-                ValueType = 'Dword'
-                ValueData = "0"
-            }
+    foreach ($s in $SchannelSubkey) {
+
+        Registry $s {
+            Ensure = 'Present'
+            Key = "HKEY_Local_Machine\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\$s"
+            ValueName = "Enabled"
+            ValueType = 'Dword'
+            ValueData = "0"
         }
+    }
+
+        #Know this probably won't work if WMF5 isn't already on the target box
+        #only using it for depends on example
+        File fWMFv5 {
+            Type = 'Directory'
+            Ensure = 'Present'
+            SourcePath ="\\fileshare\Files"
+            DestinationPath = "C:\_File_Area"
+            MatchSource = $True
+            Recurse = $True
+            #would like to include checksum
+        }
+
+        xHotfix hfWMF5 {
+            Id = 'KB3134758'
+            Path ="C:\_File_Area\Windows Management Framework 5.0 RTM\Win8.1AndW2K12R2-KB3134758-x64.msu"
+            Ensure = 'Present'
+            DependsOn = '[File]fWMFv5'
+        }
+
     }
 }
 
