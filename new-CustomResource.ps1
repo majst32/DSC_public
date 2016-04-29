24a4d6b2f5c4a350f0f99b08f1a9040283b86a28
Missy Januszko <majst32@noreply.github.com>
2016-04-29 15:18:26 -0400

    Changed DateType items to string

--- new-CustomResource.ps1
+++ new-CustomResource.ps1
@@ -1,17 +1,27 @@
-﻿new-xDscResource -Name mDHCPFailoverScope -ModuleName mDHCPFailover -path "C:\Program Files\WindowsPowershell\Modules" -property $(
+﻿new-xDscResource -Name mDHCPLBFailoverRelationship -ModuleName mDHCPFailover -path "C:\Program Files\WindowsPowershell\Modules" -property $(
+    New-xDscResourceProperty -Name "Ensure" -type String -Attribute Required -validateSet Present,Absent
     New-xDscResourceProperty -Name "RelationshipName" -Type String -Attribute Key
-    New-xDscResourceProperty -Name "Mode" -Type String -attribute Write -ValidateSet ("LoadBalancing","HotStandby")
-    #NOTE, this won't work, should be of type IPAddress, only here for practice purposes as string
-    New-xDscResourceProperty -Name "Scope" -Type String -Attribute Write
-    New-xDscResourceProperty -Name "PartnerServer" -type string -Attribute Write
+    New-xDscResourceProperty -Name "Scope" -Type String -Attribute Key
+    New-xDscResourceProperty -Name "PartnerServer" -type string -Attribute Required
+    New-xDscResourceProperty -Name "AutoStateTransition" -type Boolean -Attribute Write
+    #NOTE, can you treat this as a credential?
+    New-xDscResourceProperty -Name "SharedSecret" -type string -Attribute Write
+    New-xDscResourceProperty -Name "MCLT" -type string -Attribute Write
+    New-xDscResourceProperty -Name "LBPercentage" -Type Uint32 -Attribute Write
+    New-xDscResourceProperty -Name "StateSwitchInterval" -type string -Attribute Write
+    )
+new-xDscResource -Name mDHCPHSFailoverRelationship -ModuleName mDHCPFailover -path "C:\Program Files\WindowsPowershell\Modules" -property $(
+    New-xDscResourceProperty -Name "Ensure" -type String -Attribute Write -validateSet Present,Absent
+    New-xDscResourceProperty -Name "RelationshipName" -Type String -Attribute Key
+    New-xDscResourceProperty -Name "Scope" -Type String -Attribute Key
+    New-xDscResourceProperty -Name "PartnerServer" -type string -Attribute Required
     New-xDscResourceProperty -Name "AutoStateTransition" -type Boolean -Attribute Write
     #NOTE, can you treat this as a credential?
     New-xDscResourceProperty -Name "SharedSecret" -type string -Attribute Write
     #NOTE, what about types of DateTime vs Timespan? 
-    New-xDscResourceProperty -Name "MCLT" -type DateTime -Attribute Write
-    New-xDscResourceProperty -Name "LBPercentage" -Type Uint32 -Attribute Write
+    New-xDscResourceProperty -Name "MCLT" -type String -Attribute Write
     New-xDscResourceProperty -Name "ReservePercent" -type Uint32 -Attribute Write
     New-xDscResourceProperty -Name "ServerRole" -type String -Attribute Write
     #NOTE, same with DateTime vs Timespan
-    New-xDscResourceProperty -Name "StateSwitchInterval" -type DateTime -Attribute Write
+    New-xDscResourceProperty -Name "StateSwitchInterval" -type String -Attribute Write
     )
