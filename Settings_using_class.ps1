eb491d6271358874068e42419f1be6d044bf7bfd
Missy Januszko <majst32@noreply.github.com>
2016-05-10 14:28:05 -0400

    Added DHCPFailover2 (class-based) and class-based config Settings_using_Class.ps1

---
+++ Settings_using_class.ps1
@@ -0,0 +1,192 @@
+﻿ $ConfigData=@{
+    AllNodes = @(
+        @{
+            NodeName  = "*"
+            WSUSUrl = "http://W2:8530"
+
+        },
+        @{
+            NodeName = 'S1'
+            GUID = Get-DscLocalConfigurationManager -CimSession S1 | Select-Object -ExpandProperty ConfigurationID
+        },
+        @{
+            NodeName = 'S2'
+            Role     = @('DHCPServer','DHCPServer1')
+            GUID = Get-DscLocalConfigurationManager -CimSession S2 | Select-Object -ExpandProperty ConfigurationID
+            Partner = 'S3'
+        }, 
+        @{
+            NodeName = 'S3'
+            Role     = 'DHCPServer'
+            GUID = Get-DscLocalConfigurationManager -CimSession S3 | Select-Object -ExpandProperty ConfigurationID
+        } 
+
+    );
+}
+  
+configuration BuildOut {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration,xDHCPServer,@{moduleName="mDHCPFailover2";moduleVersion="1.2"}
+    
+    node $AllNodes.GUID {
+        
+   ###########################################START WSUS####################################
+        
+        Registry HKCU_WSUS_del_1 {
+        Key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+        ValueName = 'ProxyEnable'
+        Ensure = "Absent"
+        } 
+
+        Registry HKCU_WSUS_del_2 {
+        Key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+        ValueName = 'AutoConfigURL'
+        Ensure = "Absent"
+        DependsOn = '[Registry]HKCU_WSUS_del_1'
+        }
+
+        Registry HKCU_WSUS_del_3 {
+        Key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+        ValueName = 'ProxyServer'
+        Ensure = "Absent"
+        DependsOn = '[Registry]HKCU_WSUS_del_2'
+        }
+
+        Registry HKLM_WSUS_add_1 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+            ValueName = 'WUServer'
+            Ensure = 'Present'
+            ValueData = $Node.WSUSUrl
+            ValueType = 'String'
+            DependsOn = '[Registry]HKCU_WSUS_del_3'
+        }
+
+        Registry HKLM_WSUS_add_2 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+            ValueName = 'WUStatusServer'
+            Ensure = 'Present'
+            ValueData = $Node.WSUSUrl
+            ValueType = 'String'
+            DependsOn = '[Registry]HKLM_WSUS_add_1'
+        }
+
+        Registry HKLM_WSUS_add_3 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+            ValueName = 'NoAutoUpdate'
+            Ensure = 'Present'
+            ValueData = '1'
+            ValueType = 'Dword'
+            DependsOn = '[Registry]HKLM_WSUS_add_2'
+        }
+
+        Registry HKLM_WSUS_add_4 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+            ValueName = 'AUOptions'
+            Ensure = 'Present'
+            ValueData = '3'
+            ValueType = 'Dword'
+            DependsOn = '[Registry]HKLM_WSUS_add_3'
+        }
+
+        Registry HKLM_WSUS_add_5 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+            ValueName = 'UseWUServer'
+            Ensure = 'Present'
+            ValueData = '1'
+            ValueType = 'Dword'
+            DependsOn = '[Registry]HKLM_WSUS_add_4'
+        }
+
+        Registry HKLM_WSUS_add_6 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+            ValueName = 'ScheduledInstallDay'
+            Ensure = 'Present'
+            ValueData = '0'
+            ValueType = 'Dword'
+            DependsOn = '[Registry]HKLM_WSUS_add_5'
+        }
+
+        Registry HKLM_WSUS_add_7 {
+            Key = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+            ValueName = 'ScheduledInstallTime'
+            Ensure = 'Present'
+            ValueData = '3'
+            ValueType = 'Dword'
+            DependsOn = '[Registry]HKLM_WSUS_add_6'
+        }
+
+        Service WSUS {
+            Name = 'wuauserv'
+            DependsOn = '[Registry]HKLM_WSUS_add_7'
+            Ensure = 'Present'
+            StartupType = 'Automatic'
+            State = 'Running'
+        }
+
+
+#####After setting all the registry settings, need to recycle wuauserv, how do you do this?
+#####Can't set it to stopped and then started with a DSC resource :(
+#####Then need it to install patches########################################################
+
+#########################################END WSUS###########################################
+    
+#End Node     
+    }
+
+    node $AllNodes.Where{$_.Role -eq 'DHCPServer'}.GUID {
+
+###########################################START DHCP####################################
+#Should statically assign IP address first
+#Should set WSUS and update after
+#########################################################################################
+
+    WindowsFeature DHCP {
+        Name = 'DHCP'
+        Ensure = 'Present'
+    }
+
+    WindowsFeature DHCPMgmt {
+        Name = 'RSAT-DHCP'
+        Ensure =  'Present'
+    }
+
+    #Add DHCP service Present/Running here
+
+   }
+
+node $AllNodes.Where{$_.Role -eq 'DHCPServer1'}.GUID {
+
+    xDHCPServerScope TestScope {
+        IPEndRange = '192.168.2.254'
+        IPStartRange = '192.168.2.1'
+        Name = 'TestScope'
+        SubnetMask = '255.255.255.0'
+        AddressFamily = 'IPv4'
+        DependsOn = '[WindowsFeature]DHCP'
+        Ensure = 'Present'
+        LeaseDuration = '10'
+        State = 'Inactive'
+    }
+
+
+    mDHCPFailover2 fakeVLANScopeFailover {
+    
+    Ensure = 'Present'
+    PartnerServer = 'S3'
+    RelationshipName = 'LBFailover'
+    ScopeName = '192.168.2.0'
+    DependsOn = '[xDhcpServerScope]TestScope'
+    LBPercentage = 50
+    SharedSecret = 'blahblahblah'
+    
+    }
+
+}
+
+################Need cross-machine dependency for DHCP to be installed on both servers####
+
+###########################################END DHCP####################################
+
+ 
+}
+
+BuildOut -ConfigurationData $ConfigData -OutputPath "C:\DSC\Config"
---
+++ mDHCPFailover2/mDHCPFailover2.psm1
@@ -0,0 +1,185 @@
+﻿# Defines the values for the resource's Ensure property.
+enum Ensure
+{
+    # The resource must be absent.    
+    Absent
+    # The resource must be present.    
+    Present
+}
+
+# [DscResource()] indicates the class is a DSC resource.
+[DscResource()]
+class mDHCPFailover2
+{
+
+    # A DSC resource must define at least one key property.
+    [DscProperty(Key)]
+    [string]$RelationshipName
+
+    # Mandatory indicates the property is required and DSC will guarantee it is set.
+    [DscProperty(Mandatory)]
+    [string] $ScopeName
+
+    [DscProperty(Mandatory)]
+    [string] $PartnerServer
+
+    [DscProperty(Mandatory)]
+    [Ensure] $Ensure
+    
+    [DSCProperty()]
+    [bool] $AutoStateTransition
+
+    [DSCProperty()]
+    [string] $SharedSecret
+
+    [DSCProperty()]
+    [string] $MCLT
+
+    [DSCProperty()]
+    [int] $LBPercentage
+
+    [DSCProperty()]
+    [string] $StateSwitchInterval
+ 
+ 
+    
+    # Sets the desired state of the resource.
+    [void] Set()
+    {  
+        
+        #########################################Fix input types
+
+    #shamelessly stolen from helper function, handle later :S
+
+    [System.TimeSpan]$timeSpan = New-TimeSpan
+    $result = [System.TimeSpan]::TryParse($This.MCLT, [ref]$timeSpan)
+
+    $This.MCLT=$timeSpan
+
+    [System.TimeSpan]$timeSpan = New-TimeSpan
+    $result = [System.TimeSpan]::TryParse($This.StateSwitchInterval, [ref]$timeSpan)
+
+    $This.StateSwitchInterval=$timeSpan
+
+    ############################End fix input#############################################
+
+    If ($This.Ensure -match "Absent") {
+        write-verbose "Removing DHCP failover relationship"
+        Remove-DhcpServerv4Failover -Name $This.RelationshipName
+        write-verbose "DHCP Failover relationship removed successfully"
+        }
+    else {
+        try {
+            $null = Get-DhcpServerv4Failover -ScopeId $This.ScopeName -ErrorAction Stop
+            write-verbose "Modifying DHCP failover relationship $($This.RelationshipName)"
+            Set-DhcpServerv4Failover -Name $This.RelationshipName -SharedSecret $This.SharedSecret -AutoStateTransition $This.AutoStateTransition -MaxClientLeadTime $This.MCLT -StateSwitchInterval $This.StateSwitchInterval -LoadBalancePercent $This.LBPercentage
+            Write-Verbose "DHCP failover relationship modified successfully"
+            }
+        catch {
+            write-verbose "Adding DHCP failover relationship"
+            Add-DhcpServerv4Failover -Name $This.RelationshipName -PartnerServer $This.PartnerServer -ScopeId $This.ScopeName -SharedSecret $This.SharedSecret -MaxClientLeadTime $This.MCLT -StateSwitchInterval $This.StateSwitchInterval
+            write-verbose "DHCP failover relationship created"
+            }
+        }
+
+    }
+                
+    
+    # Tests if the resource is in the desired state.
+    [bool] Test()
+    {        
+    #########################################Fix input types
+
+    #shamelessly stolen from helper function, handle later :S
+
+    [System.TimeSpan]$timeSpan = New-TimeSpan
+    $result = [System.TimeSpan]::TryParse($This.MCLT, [ref]$timeSpan)
+
+    $This.MCLT=$timeSpan
+
+    [System.TimeSpan]$timeSpan = New-TimeSpan
+    $result = [System.TimeSpan]::TryParse($This.StateSwitchInterval, [ref]$timeSpan)
+
+    $This.StateSwitchInterval=$timeSpan
+
+        #Write-Verbose "Use this cmdlet to deliver information about command processing."
+
+        #Write-Debug "Use this cmdlet to write debug information while troubleshooting."
+
+         try {
+            $testFailover = Get-DhcpServerv4Failover -ScopeId $This.ScopeName -ErrorAction Stop
+            }
+        catch {  #If currently absent, will fall into catch
+            if ($This.Ensure -match 'Present') {
+                write-verbose "Failover relationship not present, configuration is needed."
+                return $False
+            }
+            else {
+                write-verbose "Failover relationship is not present, already in desired state."
+                return $True
+            }
+        }
+        #If currently present, check if absent is desired state
+        if ($This.Ensure -match 'Absent') {
+            write-verbose "Failover relationship is configured, desired state is unconfigured."
+            return $False
+            }
+        
+        #Otherwise failover is currently present and desired present, so check all settings.        
+        elseif ($TestFailover.Name -notmatch $This.RelationshipName) {
+            write-verbose "Failover Name does not match $($This.RelationshipName), configuration is needed."
+            return $False
+            }
+        elseif ($TestFailover.PartnerServer -notmatch $This.PartnerServer) {
+            write-verbose "Failover relationship with $($This.PartnerServer) is not found, configuration is needed."
+            return $False
+            }
+        elseif ($TestFailover.AutoStateTransition -ne $This.AutoStateTransition) {
+            Write-Verbose "Failover Auto State Transition does not match $($This.AutoStateTranstion), configuration is needed."
+            return $False
+            }
+    
+        #Cannot test SharedSecret - get-DHCPServerv4Failover does not return this value
+        #More research is needed to see if it can be checked some other way
+        #Skip checking for now
+        <#
+        elseif ($TestFailover.SharedSecret -notmatch $SharedSecret) {
+            Write-Verbose "Shared Secret does not match desired shared secret, configuration is needed."
+            return $False
+            }  #>
+
+
+         elseif (($This.MCLT -ne $Null) -and ($TestFailover.MaxClientLeadTime -ne $This.MCLT)) {  #Only check if a value is specified
+            Write-Verbose "$($TestFailover.MaxClientLeadTime) does not match $($This.MCLT)."
+            return $False
+            }
+        elseif (($This.LBPercentage -ne $Null) -and ($TestFailover.LoadBalancePercent -ne $This.LBPercentage)) { #Only check if a value is specified
+            Write-Verbose "Load Balancing Percentage does not match $($This.LBPercentage) Percent."
+            return $False
+            }
+        elseif (($This.StateSwitchInterval -ne $Null) -and ($TestFailover.StateSwitchInterval -notmatch $This.StateSwitchInterval)) { #Only check if a value is specified
+            Write-Verbose "State Switch Interval does not match $($This.StateSwitchInterval)"
+            return $False
+            }
+        else {
+            Write-Verbose "All settings configured as requested."
+            return $True
+            }
+    }
+  
+       
+     # Gets the resource's current state.
+     [mDHCPFailover2] Get()
+     {        
+        $relationship = Get-DhcpServerv4Failover -erroraction silentlyContinue | Where-Object {$_.PartnerServer -match $PartnerServer}
+
+        if ($relationship.Name -match "*$($This).RelationshipName*") {
+            Write-Verbose "Failover relationship exists."
+        }
+        else {
+            Write-Verbose "Failover relationship does not exist, not in desired state."
+            $this = $null
+        }
+        return $this 
+    }    
+}
