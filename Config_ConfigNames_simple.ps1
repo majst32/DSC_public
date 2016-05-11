6f5353e9c3726276a2d4583256b7ee2934f435f1
Missy Januszko <majst32@noreply.github.com>
2016-05-11 15:31:11 -0400

    New config Config_ConfigNames_simple.ps1

---
+++ Config_ConfigNames_simple.ps1
@@ -0,0 +1,28 @@
+﻿$ConfigData=@{
+    AllNodes = 
+    @(
+        @{
+            NodeName  = "*"
+        },
+        @{
+            NodeName = "S1";
+            Role     = "TestConfig"
+        }     
+    )  
+}
+
+configuration TestConfig
+{
+  
+    Import-DscResource -ModuleName PSDesiredStateConfiguration 
+    node $AllNodes.Where{$_.Role -eq "TestConfig"}.Role
+    {
+        WindowsFeature WindowsBackup
+        {
+            Name   = 'Windows-Server-Backup'
+            Ensure = 'Present'
+        }
+    }
+}
+
+TestConfig -ConfigurationData $ConfigData -OutputPath "C:\DSC\Config"
