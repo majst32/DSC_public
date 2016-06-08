1ab45456f4706afbb4ef6686b27d47ed7c3c5eac
Missy Januszko <majst32@do-not-reply.com>
2016-06-07 22:28:43 -0400

    Added auth_stuff.ps1 to lab buildout

---
+++ Lab buildout/Auth_stuff.ps1
@@ -0,0 +1,4 @@
+﻿Install-WindowsFeature -Name RSAT-AD-Tools,RSAT-ADCS,RSAT-DHCP,RSAT-DNS-Server,RSAT-DFS-Mgmt-Con
+find-module PSWindowsUpdate | install-module
+import-module PSWindowsUpdate
+Get-WUInstall -IgnoreUserInput -verbose
